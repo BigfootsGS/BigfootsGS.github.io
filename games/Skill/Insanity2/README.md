3a3f5a3ad10fdf4c5f39939874c7f84da905bc23
Bigfoot9999 <80417201+Bigfoot9999@users.noreply.github.com>
2022-03-29 09:12:27 -0600

    Delete games/Skill/Insanity2 directory

--- games/Skill/Insanity2/README.md
+++
@@ -1,3 +0,0 @@
-# Insanity 2
-
-A 2D Phaser web game! Play it at [https://insanity2.ethanbaker.dev](https://insanity2.ethanbaker.dev).
--- games/Skill/Insanity2/serve.js
+++
@@ -1,6 +0,0 @@
-const browserSync = require('browser-sync')
-
-// Broser sync to serve the site
-browserSync({server: true, files: "./index.html"}, function(err, bs) {
-    console.log(bs.options.getIn(["urls", "local"]))
-})

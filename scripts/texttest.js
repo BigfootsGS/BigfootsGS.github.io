988f5b3fa250b339d2cd9dcbaf1d2979d570a252
Bigfoot9999 <80417201+Bigfoot9999@users.noreply.github.com>
2022-06-04 20:55:46 -0600

    Delete texttest.js

--- scripts/texttest.js
+++
@@ -1,5 +0,0 @@
-document.write(
-  '<span class="textjs"; style="font-weight: 1000;font-style: normal; text-align:center; color: white; font-family: "Press Start 2P";>' +
-    r_text[i] +
-    "</span>"
-);

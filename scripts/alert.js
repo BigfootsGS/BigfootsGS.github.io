4d4bd28a29e40f5dd485243e3de20c6a2d8f44e5
Bigfoot9999 <bigfoot.gaming9999@gmail.com>
2022-10-06 09:42:10 -0600

    Add about:blank cloak to settings page

--- scripts/alert.js
+++ scripts/alert.js
@@ -101,13 +101,3 @@ else {
 
 
 //alert
-function promptalert() {
-                if (confirm("Do you want to open this website in an about:blank page? \ Press ok to open website in a blank tab! \ Press Cancel to continue to normal website!")) {
-                    txt = "You pressed OK!";
-                    blank();
-                } 
-                else {
-                    txt = "You pressed Cancel!";
-                }
-                console.log(txt);
-            }

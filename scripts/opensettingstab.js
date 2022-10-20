487852b4c4e37e12e5bc6e3b21c1b7a59c2da191
Bigfoot9999 <bigfoot.gaming9999@gmail.com>
2022-10-20 16:29:37 -0600

    gg

---
+++ scripts/opensettingstab.js
@@ -0,0 +1,8 @@
+function opensettings(){
+    const id = document.getElementById("opensettingsid");
+    if (id.style.display === "none") {
+        id.style.display = "block";
+      } else {
+        id.style.display = "none";
+      }
+}

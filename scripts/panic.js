70909d666e4ec51610ceae21fae99f2eddc1d847
Bigfoot9999 <bigfoot.gaming9999@gmail.com>
2022-11-21 13:30:57 -0700

    Move games to seperate javascript file

---
+++ scripts/panic.js
@@ -0,0 +1,8 @@
+
+
+    console.warn(
+        "%cREAD ME!",
+        "color: black; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
+        "If you know html, css, js, or just know a ton of games, we might be able to add you to our dev team! We have tons of benefits, including custom graphics for your website/logos, etc. All the graphics are custom, except for the bigfoot in our logo. Im not that good at detials lol. We also need people to add thumbnails to our main page. As you can probably tell, we need some help. If you are just trying to steal my games, they are on github. Just give me credit if you use my code (like the array code). I can tell if you steal it btw. Anyway have a good day :)"
+      );
+  

ff0d0dfaac4a12d4c7825db591b374a5733d5f99
Bigfoot9999 <bigfoot.gaming9999@gmail.com>
2023-02-10 10:46:58 -0700

    Add Loading Screen

--- scripts/tab.js
+++ scripts/tab.js
@@ -65,3 +65,23 @@ changeTabTitle();
 document.getElementById("console-output").style.color = "green";
 console_output.innerText = "Preset applied successfully!"
 }
+
+
+
+const wait = (delay = 0) =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) => 
+  (typeof elementOrSelector === 'string'
+    ? document.querySelector(elementOrSelector)
+    : elementOrSelector
+  ).style.display = visible ? 'block' : 'none';
+
+setVisible('.page', false);
+setVisible('#loading', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+  wait(1000).then(() => {
+    setVisible('.page', true);
+    setVisible('#loading', false);
+  }));
